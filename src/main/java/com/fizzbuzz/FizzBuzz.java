78a8800a12616dcc87a12bb36ca4ffd19e63135b
Ioana <ioana3popescu@gmail.com>
2026-07-12 21:09:46 +0200

    feat: add FizzBuzz ejercise logic

--- src/main/java/com/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/fizzbuzz/FizzBuzz.java
@@ -1,5 +1,26 @@
 package com.fizzbuzz;
 
 public class FizzBuzz {
-    
+
+    public String convert(int number) {
+        String numberAsText = String.valueOf(number);
+
+        if (
+            (number % 3 == 0 || numberAsText.contains("3"))
+            &&
+            (number % 5 == 0 || numberAsText.contains("5"))
+        ) {
+            return "FizzBuzz";
+        }
+
+        if (number % 3 == 0 || numberAsText.contains("3")) {
+            return "Fizz";
+        }
+
+        if (number % 5 == 0 || numberAsText.contains("5")) {
+            return "Buzz";
+        }
+
+        return numberAsText;
+    }
 }
