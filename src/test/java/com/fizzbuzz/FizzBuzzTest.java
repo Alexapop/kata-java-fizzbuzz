8b55e883fcd68a92dd84fb8ff707989e019654d6
Ioana <ioana3popescu@gmail.com>
2026-07-13 15:39:14 +0200

    test: add unit tests and coverage report

--- src/test/java/com/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/fizzbuzz/FizzBuzzTest.java
@@ -1,14 +1,44 @@
 package com.fizzbuzz;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class FizzBuzzTest {
 
     @Test
-    public void test_fizzbuzz_example() {
-        assertTrue(true);
+    public void testReturnsFizzBuzzWhenDivisibleBy15() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertThat(fizzBuzz.convert(15), is("FizzBuzz"));
     }
 
+    @Test
+    public void testReturnsFizzWhenDivisibleBy3() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertThat(fizzBuzz.convert(3), is("Fizz"));
+    }
+
+    @Test
+    public void testReturnsBuzzWhenDivisibleBy5() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertThat(fizzBuzz.convert(5), is("Buzz"));
+    }
+
+    @Test
+    public void testReturnsFizzWhenContains3() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertThat(fizzBuzz.convert(23), is("Fizz"));
+    }
+
+    @Test
+    public void testReturnsBuzzzWhenContains5() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertThat(fizzBuzz.convert(59), is("Buzz"));
+    }
+
+    @Test
+    public void testReturnsNumberWhenNoConditionIsMet() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertThat(fizzBuzz.convert(11), is("11"));
+    }
 }
