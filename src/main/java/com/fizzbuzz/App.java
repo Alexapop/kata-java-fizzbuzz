3e81bd1b250b1dadd974ed44487837886a1ff215
Ioana <ioana3popescu@gmail.com>
2026-07-14 08:59:36 +0200

    feat: print results from 1 to 100

--- src/main/java/com/fizzbuzz/App.java
+++ src/main/java/com/fizzbuzz/App.java
@@ -1,13 +1,10 @@
 package com.fizzbuzz;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        for (int number = 1; number <= 100; number++) {
+            System.out.println(fizzBuzz.convert(number));
+        }
     }
 }
